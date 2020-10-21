58ddbf67ad6ce5c4af663e0f372d9bf08cc21a30
huang <qqjay2013@gmail.com>
2020-10-21 20:52:48 +0800

    新接口

---
+++ mocks/datamng/config/connection/get.js
@@ -0,0 +1,15 @@
+/**
+ * 数据源列表
+ */
+
+module.exports = {
+    code: 200,
+    msg: 'ok',
+    data: [
+        {
+            connectionId:'主键',
+            connectionName:'数据源名称',
+            connectionType:'jdbc',
+        },
+    ]
+};
---
+++ mocks/datamng/config/connection/post.js
@@ -0,0 +1,15 @@
+/**
+ * 新增数据源
+ */
+
+module.exports = {
+    code: 200,
+    msg: 'ok',
+    data: [
+        {
+            connectionId:'主键',
+            connectionName:'数据源名称',
+            connectionType:'jdbc',
+        },
+    ]
+};
