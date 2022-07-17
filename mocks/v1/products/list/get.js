0ec3c8bc2172753b391834a05b68b994e3de4b18
Boris <qqjay2013@gmail.com>
2022-07-17 10:58:41 +0800

    feat: 接口完善

--- mocks/v1/products/list/get.js
+++ mocks/v1/products/list/get.js
@@ -4,19 +4,35 @@ module.exports = (ctx)=>{
         current=1,
 
     } = ctx.query;
-    const total = 40
+    const total = 88
     pageSize = Number(pageSize)
     current = Number(current)
-    if(pageSize*current>total){
+    const data = []
+    const last = total - pageSize*current
+    if(last<0 ){
+       
+
+
+        for (let i = 0; i <(total&10); i++) {
+            const id = (current-1)*pageSize+i+1
+            data[i] = {
+                id:id,
+                name: id+'name',
+            }
+            
+        }
+
+      
         return {
             pageSize: pageSize,
-        current: current,
-        total: total,
-        data: []
+            current: current,
+            total: total,
+            data: data
         }
 
     }
-    const data = Array(pageSize);
+    
+
     for (let i = 0; i <pageSize; i++) {
         const id = (current-1)*pageSize+i+1
         data[i] = {
